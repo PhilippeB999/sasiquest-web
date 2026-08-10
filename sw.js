1a5a35a2616d1692108a4703c2594fd92d398d4d
Productions iMédias <philippe.beaubien@gmail.com>
2026-08-10 15:38:16 -0400

    Licence: code unique par app

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Au premier chargement, il met en cache tous les fichiers de l'app.
    Ensuite, l'app se lance sans réseau, exactement comme une app native. */
 
-const CACHE = "sasiquest-v8";
+const CACHE = "sasiquest-v9";
 const ASSETS = [
   "index.html",
   "app.js",
